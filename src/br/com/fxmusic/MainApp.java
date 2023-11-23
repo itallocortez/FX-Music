e7d718f30a7c6f64c2a9277d2bdcebd1beb4b227
Itallo Cortez <itallo.cortez.703@ufrn.edu.br>
2023-11-23 14:37:01 -0300

    Configurando git e pacotes

---
+++ src/br/com/fxmusic/MainApp.java
@@ -0,0 +1,25 @@
+package br.com.fxmusic;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("visao/TelaPrincipal.fxml"));
+		Scene scene = new Scene(root);
+		
+		stage.setScene(scene);
+		stage.setTitle("FX Music");
+		stage.setResizable(false);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
